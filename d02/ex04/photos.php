4d83e0b9b9959475c1b26f34cb45e7f1f5e4cdf7
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-12 20:46:50 +0200

    fixed regex for finding images from html. directory name regex is still broken.

--- d02/ex04/photos.php
+++ d02/ex04/photos.php
@@ -7,10 +7,9 @@ if (!$html)
 	echo "not valid url\n";
 	return;
 }
-
 preg_match_all("/<img(?:[^>]*)src=\"([^\"]*)/", $html, $matches);
-var_dump($matches);
-
+foreach($matches[1] as $i => $match)
+	$matches[1][$i] = preg_replace("/amp;/", "", $match);
 if (!$matches[1])
 {
 	echo "no images found\n";
@@ -19,11 +18,19 @@ if (!$matches[1])
 
 $pattern = "/http:\/\//";
 $dirname = preg_replace($pattern, "", $url);
+$pattern = "/https:\/\//";
+$dirname = preg_replace($pattern, "", $dirname);
+$dirname = "asd";
 if (!is_dir($dirname))
+{
+	echo "making dir ".$dirname;
 	mkdir($dirname);
-
+}
 $i = 0;
-foreach ($matches[1] as $link)
+preg_match_all("/<img(?:[^>]*)src[^\/]*[\/]*([^\"?]*)/", $html, $names);
+foreach ($names[1] as $i => $name)
+	$names[1][$i] = preg_replace("/(.*\/)/", "", $name);
+foreach ($matches[1] as $i => $link)
 {
 	$img;
 	if ($link[0] == '/')
@@ -31,9 +38,9 @@ foreach ($matches[1] as $link)
 	else
 		$img = file_get_contents($link);
 	if ($img)
-		file_put_contents("./$dirname/$i.jpg", $img);
+		file_put_contents("./$dirname/".$names[1][$i], $img);
+	if (!$names[1][$i])
+		echo "error more images than image names found.\n";
 	$i++;
 }
-// print_r($matches);
 ?>
-
