3cf471e42a67bcedaa9bf254d7c5985cb7472500
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-12 15:22:24 +0200

    used a modified version of ssap2 in ssap because sorting was wrong for some cases

--- d01/ex06/ssap.php
+++ d01/ex06/ssap.php
@@ -2,12 +2,29 @@
 <?PHP
 if ($argc < 2)
 	return (0);
-for ($i = 0; $i < $argc; $i++) {
-	$arg = explode(" ", $argv[$i]);
-	$arg = array_filter($arg);
-	$index = 0;
-	foreach ($arg as $str)
-		echo "$str\n";
+$nums = [];
+$chars = [];
+$else = [];
+for ($i = 1; $i < $argc; $i++)
+{
+	$array = explode(" ", $argv[$i]);
+	$array = array_filter($array);
+	for ($j = 0; $j < count($array); $j++)
+	{
+		if (is_numeric($array[$j][0]))
+			array_push($nums, $array[$j]);
+		else if (($array[$j][0] >= 'A' && $array[$j][0] <= 'Z') ||
+				($array[$j][0] >= 'a' && $array[$j][0] <= 'z'))
+			array_push($chars, $array[$j]);
+		else
+			array_push($else, $array[$j]);
+	}
 }
-
+sort($chars);
+sort($nums);
+sort($else);
+$res = array_merge($chars, $nums, $else);
+foreach ($res as $str) {
+	echo "$str\n";
+}
 ?>
