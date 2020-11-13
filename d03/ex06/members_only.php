902fe7a028379faf3ea25e7b18e01be49231bd4f
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-13 21:07:54 +0200

    moved header function call to allow access past the login pop up and added a message 'Hello Zaz'

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -1,15 +1,18 @@
 <?PHP
-header('WWW-Authenticate: Basic realm="Member area"');
-header('HTTP/1.0 401 Unauthorized');
-header("X-Frame-Options: none");
 if ($_SERVER[PHP_AUTH_USER] == "zaz" &&
 	$_SERVER[PHP_AUTH_PW] == "Ilovemylittlepony")
 {
 	$image = file_get_contents("../img/42.png");
 	$image = base64_encode($image);
-	echo "<img src='data:image/png;base64,$image'></img>";
+	echo "<html><body>
+Hello Zaz<br />
+<img src='data:image/png;base64,$image'></img>
+</body></html>\n";
 }
 else
-	echo "<html><body>That area is accessible for members only</body></html>     ";
+{
+	header('WWW-Authenticate: Basic realm="Member area"');
+	header('HTTP/1.0 401 Unauthorized');
+	echo "<html><body>That area is accessible for members only</body></html>     \n";
+}
 ?>
-
