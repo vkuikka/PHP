3edc93869590a7e0f242ad7eb6f15e50f847afa5
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-13 15:00:57 +0200

    A function that is not allowed was being used before

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -2,12 +2,8 @@
 header('WWW-Authenticate: Basic realm="Member area"');
 header('HTTP/1.0 401 Unauthorized');
 header("X-Frame-Options: none");
-if (!isset($_SERVER[PHP_AUTH_USER]))
-{
-	echo "pressed cancel";
-}
-else if ($_SERVER[PHP_AUTH_USER] == "zaz" &&
-		$_SERVER[PHP_AUTH_PW] == "Ilovemylittlepony")
+if ($_SERVER[PHP_AUTH_USER] == "zaz" &&
+	$_SERVER[PHP_AUTH_PW] == "Ilovemylittlepony")
 {
 	$image = file_get_contents("../img/42.png");
 	$image = base64_encode($image);
