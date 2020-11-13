992b01116727623dd41fcbe32d13dad1f0ccf0fd
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-13 20:10:45 +0200

    changed time to zero in setcookie

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -2,7 +2,7 @@
 if ($_GET["action"] == "set")
 	setcookie($_GET["name"], $_GET["value"], time()+3600);
 if ($_GET["action"] == "del")
-	setcookie($_GET["name"], "", time() - 1);
+	setcookie($_GET["name"], "", 0);
 if ($_GET["action"] == "get")
 {
 	$val = $_COOKIE[$_GET["name"]];
