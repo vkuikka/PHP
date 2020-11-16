c915de248fba5dcbb472fa094bceef17aaaa7cef
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-16 19:05:21 +0200

    added some class testing to d06/ex01

---
+++ d06/ex01/Vertex.class.php
@@ -0,0 +1,60 @@
+#!/usr/bin/php
+<?php
+	class Color {
+		public $red;
+		public $green;
+		public $blue;
+		static $verbose = false;
+
+		public $func = 'testfunc';
+
+		function __construct(array $colors){
+			$this->red = $colors[0];
+			$this->green = $colors[1];
+			$this->blue = $colors[2];
+			echo "construct\n";
+		}
+		function __destruct(){
+			echo "destruct\n";
+		}
+		function __toString():string{
+			return "tostring\n";
+		}
+		function doc(){
+			echo "documentation\n";
+		}
+		function add(Color $other):Color{
+			$new = new Color(array(0, 0, 0));
+			$new->red = $this->red + $other->red;
+			$new->green = $this->green + $other->green;
+			$new->blue = $this->blue + $other->blue;
+			return $new;
+		}
+		function sub(Color $other):Color{
+			$new = new Color(array(0, 0, 0));
+			$new->red = $this->red - $other->red;
+			$new->green = $this->green - $other->green;
+			$new->blue = $this->blue - $other->blue;
+			return $new;
+		}
+		function mult(Color $other):Color{
+			$new = new Color(array(0, 0, 0));
+			$new->red = $this->red * $other->red;
+			$new->green = $this->green * $other->green;
+			$new->blue = $this->blue * $other->blue;
+			return $new;
+		}
+	}
+	$arr = [1, 2, 3];
+	$obj = new Color($arr);
+
+	var_dump($obj->blue);
+	$new = $obj->add($obj);
+	$new = $new->sub($obj);
+
+	// var_dump($obj->blue);
+	// var_dump($new);
+
+
+	return;
+?>
