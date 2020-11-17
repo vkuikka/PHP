e8c46939a0d112e7a5a120a07974d7eefee4f1db
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-17 13:55:19 +0200

    added color.class

--- d06/ex00/Color.class.php
+++ d06/ex00/Color.class.php
@@ -6,52 +6,60 @@
 		static $verbose = false;
 
 		function __construct(array $colors){
-			$this->red = $colors[0];
-			$this->green = $colors[1];
-			$this->blue = $colors[2];
-			echo "construct\n";
+			if (isset($colors['rgb']))
+			{
+				$col = dechex($colors['rgb']);
+				while (strlen($col) != 6)
+					$col = "0".$col;
+				$this->red = hexdec($col[0].$col[1]);
+				$this->green = hexdec($col[2].$col[3]);
+				$this->blue = hexdec($col[4].$col[5]);
+			}
+			else if (isset($colors['red']) &&
+					isset($colors['green']) &&
+					isset($colors['blue']))
+			{
+				$this->red = $colors['red'];
+				$this->green = $colors['green'];
+				$this->blue = $colors['blue'];
+			}
+			if (self::$verbose == true)
+				printf("Color( red: %3d, green: %3d, blue: %3d ) constructed\n",
+							$this->red, $this->green, $this->blue);
 		}
 		function __destruct(){
-			echo "destruct\n";
+			if (self::$verbose == true)
+				printf("Color( red: %3d, green: %3d, blue: %3d ) destructed\n",
+							$this->red, $this->green, $this->blue);
 		}
 		function __toString():string{
-			return "tostring\n";
+			return (sprintf("Color( red: %3d, green: %3d, blue: %3d )",
+						$this->red, $this->green, $this->blue));
 		}
 		static function doc(){
-			echo "documentation\n";
+			if (file_exists("Color.doc.txt"))
+				echo file_get_contents("Color.doc.txt");
 		}
 		function add(Color $other):Color{
-			$new = new Color(array(0, 0, 0));
-			$new->red = $this->red + $other->red;
-			$new->green = $this->green + $other->green;
-			$new->blue = $this->blue + $other->blue;
-			return $new;
+			$new['red'] = $this->red + $other->red;
+			$new['green'] = $this->green + $other->green;
+			$new['blue'] = $this->blue + $other->blue;
+			$res = new Color($new);
+			return $res;
 		}
 		function sub(Color $other):Color{
-			$new = new Color(array(0, 0, 0));
-			$new->red = $this->red - $other->red;
-			$new->green = $this->green - $other->green;
-			$new->blue = $this->blue - $other->blue;
-			return $new;
+			$new['red'] = $this->red - $other->red;
+			$new['green'] = $this->green - $other->green;
+			$new['blue'] = $this->blue - $other->blue;
+			$res = new Color($new);
+			return $res;
 		}
-		function mult(Color $other):Color{
-			$new = new Color(array(0, 0, 0));
-			$new->red = $this->red * $other->red;
-			$new->green = $this->green * $other->green;
-			$new->blue = $this->blue * $other->blue;
-			return $new;
+		function mult(int $mult):Color{
+			$new['red'] = $this->red * $mult;
+			$new['green'] = $this->green * $mult;
+			$new['blue'] = $this->blue * $mult;
+			$res = new Color($new);
+			return $res;
 		}
 	}
-	$arr = [1, 2, 3];
-	$obj = new Color($arr);
-
-	var_dump($obj->blue);
-	$new = $obj->add($obj);
-	$new = $new->sub($obj);
-
-	// var_dump($obj->blue);
-	// var_dump($new);
-
-
-	return;
 ?>
