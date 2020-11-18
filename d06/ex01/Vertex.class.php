86fe9e099d9189a795bb3b389bcade8e9e5dfcba
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-18 18:21:13 +0200

    fixed ex01 in accordance to the subject pdf

--- d06/ex01/Vertex.class.php
+++ d06/ex01/Vertex.class.php
@@ -1,58 +1,57 @@
 <?php
-	require_once "../ex00/Color.class.php";
+	require_once "Color.class.php";
 
 	class Vertex {
-		public $x;
-		public $y;
-		public $z;
-		public $col;
-		public $w = 1.0;
+		private	$_x;
+		private	$_y;
+		private	$_z;
+		private	$_w = 1.0;
+		private	$_col;
+		static	$verbose = false;
 
-		static $verbose = false;
-
-
-		public function getX(){return $this->x;}
-		public function getY(){return $this->y;}
-		public function getZ(){return $this->z;}
-		public function getW(){return $this->w;}
+		public function getX(){return $this->_x;}
+		public function getY(){return $this->_y;}
+		public function getZ(){return $this->_z;}
+		public function getW(){return $this->_w;}
+		public function getColor(){return $this->_col;}
 		function __construct(array $vertex){
 			if (isset($vertex['color']))
-				$this->col = $vertex['color'];
+				$this->_col = $vertex['color'];
 			else
-				$this->col = new Color( array('red' => 255, 'green' => 255, 'blue' => 255));
+				$this->_col = new Color( array('red' => 255, 'green' => 255, 'blue' => 255));
 
 			if (isset($vertex['x']) &&
 				isset($vertex['y']) &&
 				isset($vertex['z']))
 			{
-				$this->x = $vertex['x'];
-				$this->y = $vertex['y'];
-				$this->z = $vertex['z'];
+				$this->_x = $vertex['x'];
+				$this->_y = $vertex['y'];
+				$this->_z = $vertex['z'];
 				if (isset($vertex['w']))
-					$this->w = $vertex['w'];
+					$this->_w = $vertex['w'];
 				else
-					$this->w = 1.0;
+					$this->_w = 1.0;
 			}
 			if (self::$verbose == true)
 			{
-				printf("Vertex( x: %.2f, y: %.2f, z: %.2f, w: %.2f, ",
-							$this->x, $this->y, $this->z, $this->w);
-				print $this->col." ) constructed\n";
+				printf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f, ",
+							$this->_x, $this->_y, $this->_z, $this->_w);
+				print $this->_col." ) constructed\n";
 			}
 		}
 		function __destruct(){
 			if (self::$verbose == true)
 			{
-				printf("Vertex( x: %.2f, y: %.2f, z: %.2f, w: %.2f, ",
-						$this->x, $this->y, $this->z, $this->w);
-				print $this->col." ) destructed\n";
+				printf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f, ",
+						$this->_x, $this->_y, $this->_z, $this->_w);
+				print $this->_col." ) destructed\n";
 			}
 		}
 		function __toString():string{
-			$str = sprintf("Vertex( x: %.2f, y: %.2f, z: %.2f, w: %.2f",
-						$this->x, $this->y, $this->z, $this->w);
-			if (self::$verbose === true && isset($this->col))
-				$str = $str.", ".$this->col;
+			$str = sprintf("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f",
+						$this->_x, $this->_y, $this->_z, $this->_w);
+			if (self::$verbose === true && isset($this->_col))
+				$str = $str.", ".$this->_col;
 			$str .= " )";
 			return $str;
 		}
@@ -62,26 +61,26 @@
 		}
 
 		function add(Color $other):Color{
-			$new['x'] = $this->x + $other->x;
-			$new['y'] = $this->y + $other->y;
-			$new['z'] = $this->z + $other->z;
-			$new['w'] = $this->w + $other->w;
+			$new['x'] = $this->_x + $other->_x;
+			$new['y'] = $this->_y + $other->_y;
+			$new['z'] = $this->_z + $other->_z;
+			$new['w'] = $this->_w + $other->_w;
 			$res = new Vertex($new);
 			return $res;
 		}
 		function sub(Color $other):Color{
-			$new['x'] = $this->x - $other->x;
-			$new['y'] = $this->y - $other->y;
-			$new['z'] = $this->z - $other->z;
-			$new['w'] = $this->w - $other->w;
+			$new['x'] = $this->_x - $other->_x;
+			$new['y'] = $this->_y - $other->_y;
+			$new['z'] = $this->_z - $other->_z;
+			$new['w'] = $this->_w - $other->_w;
 			$res = new Vertex($new);
 			return $res;
 		}
 		function mult(int $mult):Color{
-			$new['x'] = $this->x * $mult;
-			$new['y'] = $this->y - $mult;
-			$new['z'] = $this->z - $mult;
-			$new['w'] = $this->w - $mult;
+			$new['x'] = $this->_x * $mult;
+			$new['y'] = $this->_y * $mult;
+			$new['z'] = $this->_z * $mult;
+			$new['w'] = $this->_w * $mult;
 			$res = new Vertex($new);
 			return $res;
 		}
