30b4ce4c2c06e5e5ea0f0c7e3856723cdca201f5
Ville Kuikka <vkuikka@student.hive.fi>
2020-11-18 20:21:15 +0200

    added previous classes to ex04

---
+++ d06/ex04/Camera.class.php
@@ -0,0 +1,141 @@
+<?php
+	require_once 'Vertex.class.php';
+	require_once 'Vector.class.php';
+
+	class Matrix {
+		const IDENTITY = "ID";
+		const SCALE = "SC";
+		const RX = "RX";
+		const RY = "RY";
+		const RZ = "RZ";
+		const TRANSLATION = "TR";
+		const PROJECTION = "PR";
+
+		private $matrix = array();
+
+		static $verbose = false;
+
+		public function getvX(){return $this->__vtcX;}
+		public function getvY(){return $this->__vtcY;}
+		public function getvZ(){return $this->__vtcZ;}
+
+		function __construct(array $arr){
+			if ($arr['preset'] == self::IDENTITY)
+			{
+				$this->matrix[0] = [1, 0, 0, 0];
+				$this->matrix[1] = [0, 1, 0, 0];
+				$this->matrix[2] = [0, 0, 1, 0];
+				$this->matrix[3] = [0, 0, 0, 1];
+				echo "Matrix IDENTITY instance constructed\n";
+			}
+			if ($arr['preset'] == self::SCALE && isset($arr['scale']))
+			{
+				$s = $arr['scale'];
+				$this->matrix[0] = [$s,	0,	0,	0];
+				$this->matrix[1] = [0,	$s,	0,	0];
+				$this->matrix[2] = [0,	0,	$s,	0];
+				$this->matrix[3] = [0,	0,	0,	1];
+				echo "Matrix SCALE preset instance constructed\n";
+			}
+			if ($arr['preset'] == self::RX && isset($arr['angle']))
+			{
+				$c = cos($arr['angle']);
+				$s = sin($arr['angle']);
+				$this->matrix[0] = [1,	0,	0,	0];
+				$this->matrix[1] = [0,	$c,	$s,	0];
+				$this->matrix[2] = [0,	-$s,$c,	0];
+				$this->matrix[3] = [0,	0,	0,	1];
+				echo "Matrix Ox ROTATION preset instance constructed\n";
+			}
+			if ($arr['preset'] == self::RY && isset($arr['angle']))
+			{
+				$c = cos($arr['angle']);
+				$s = sin($arr['angle']);
+				$this->matrix[0] = [$c,	0,	-$s,0];
+				$this->matrix[1] = [0,	1,	0,	0];
+				$this->matrix[2] = [$s,	0,	$c,	0];
+				$this->matrix[3] = [0,	0,	0,	1];
+				echo "Matrix Oy ROTATION preset instance constructed\n";
+			}
+			if ($arr['preset'] == self::RZ && isset($arr['angle']))
+			{
+				$c = cos($arr['angle']);
+				$s = sin($arr['angle']);
+				$this->matrix[0] = [$c,	$s,	0,	0];
+				$this->matrix[1] = [-$s,$c,	0,	0];
+				$this->matrix[2] = [0,	0,	1,	0];
+				$this->matrix[3] = [0,	0,	0,	1];
+				echo "Matrix Oz ROTATION preset instance constructed\n";
+			}
+			if ($arr['preset'] == self::TRANSLATION && isset($arr['vtc']))
+			{
+				$v = $arr['vtc'];
+				$this->matrix[0] = [1,	0,	0,	0];
+				$this->matrix[1] = [0,	1,	0,	0];
+				$this->matrix[2] = [0,	0,	1,	0];
+				$this->matrix[3] = [$v->getX(),	$v->getY(),	$v->getZ(),	$v->getW()];
+				echo "Matrix TRANSLATION preset instance constructed\n";
+			}
+			if ($arr['preset'] == self::PROJECTION && isset($arr['ratio']))
+			{
+				$this->matrix[1][1] = 1 / tan(0.5 * deg2rad($arr['fov']));
+				$this->matrix[0][0] = $this->matrix[1][1] / $arr['ratio'];
+				$this->matrix[2][2] = -1 * ((float)-$arr['near'] - (float)$arr['far']) / (float)($arr['near'] - (float)$arr['far']);
+				$this->matrix[2][3] = -1;
+				$this->matrix[3][2] = (2 * (float)$arr['near'] * (float)$arr['far']) / (float)($arr['near'] - (float)$arr['far']);
+				$this->matrix[3][3] = 0;
+				echo "Matrix PROJECTION preset instance constructed\n";
+			}
+		}
+		function __toString():string{
+			$m = $this->matrix;
+			return("M | vtcX | vtcY | vtcZ | vtxO
+-----------------------------
+x | ".sprintf("%.2f", $m[0][0])." | ".sprintf("%.2f", $m[1][0])." | ".sprintf("%.2f", $m[2][0])." | ".sprintf("%.2f", $m[3][0])."
+y | ".sprintf("%.2f", $m[0][1])." | ".sprintf("%.2f", $m[1][1])." | ".sprintf("%.2f", $m[2][1])." | ".sprintf("%.2f", $m[3][1])."
+z | ".sprintf("%.2f", $m[0][2])." | ".sprintf("%.2f", $m[1][2])." | ".sprintf("%.2f", $m[2][2])." | ".sprintf("%.2f", $m[3][2])."
+w | ".sprintf("%.2f", $m[0][3])." | ".sprintf("%.2f", $m[1][3])." | ".sprintf("%.2f", $m[2][3])." | ".sprintf("%.2f", $m[3][3]));
+		}
+		function __destruct(){
+			if (self::$verbose == true)
+				print "Matrix instance destructed\n";
+		}
+		static function doc(){
+			if (file_exists("Vector.doc.txt"))
+				echo file_get_contents("Vector.doc.txt");
+		}
+		function magnitude():float{
+			return (sqrt($this->getX()**2 + $this->getY()**2 + $this->getZ()**2));
+		}
+
+		function mult(Matrix $rhs):Matrix{
+            $res = new Matrix(array());
+			for ($i = 0; $i < 4; $i++) {
+				for ($j = 0; $j < 4; $j++) {
+					$res->matrix[$i][$j] = 0;
+					$res->matrix[$i][$j] += $this->matrix[0][$j] * $rhs->matrix[$i][0];
+					$res->matrix[$i][$j] += $this->matrix[1][$j] * $rhs->matrix[$i][1];
+					$res->matrix[$i][$j] += $this->matrix[2][$j] * $rhs->matrix[$i][2];
+					$res->matrix[$i][$j] += $this->matrix[3][$j] * $rhs->matrix[$i][3];
+				}
+			}
+			return $res;
+		}
+		function transformVertex(Vertex $vtx):Vertex{
+			$m = $this->matrix;
+			$vx = $vtx->getX();
+			$vy = $vtx->getY();
+			$vz = $vtx->getZ();
+			$vw = $vtx->getW();
+			if ($vw == NULL)
+				$vw = 1;
+			$tmp = array();
+			$tmp['x'] = ($vx * $m[0][0]) + ($vy * $m[1][0]) + ($vz * $m[2][0]) + ($vw * $m[3][0]);
+			$tmp['y'] = ($vx * $m[0][1]) + ($vy * $m[1][1]) + ($vz * $m[2][1]) + ($vw * $m[3][1]);
+			$tmp['z'] = ($vx * $m[0][2]) + ($vy * $m[1][2]) + ($vz * $m[2][2]) + ($vw * $m[3][2]);
+			$tmp['w'] = ($vx * $m[0][3]) + ($vy * $m[1][3]) + ($vz * $m[2][3]) + ($vw * $m[3][3]);
+			$res = new Vertex($tmp);
+			return $res;
+		}
+	}
+?>
---
+++ d06/ex04/Vector.class.php
@@ -0,0 +1,124 @@
+<?php
+	require_once 'Vertex.class.php';
+
+	class Vector {
+		private $_x;
+		private $_y;
+		private $_z;
+		private $_w = 1.0;
+
+		static $verbose = false;
+
+		public function getX(){return $this->_x;}
+		public function getY(){return $this->_y;}
+		public function getZ(){return $this->_z;}
+		public function getW(){return $this->_w;}
+
+		function __construct(array $arr){
+			if (!isset($arr['dest']))
+			{
+				echo "ERROR give 'dest' vertex for vector. Following was given:\n";
+				var_dump($arr);
+				exit;
+			}
+			if (!isset($arr['orig']))
+				$arr['orig'] = new Vertex(array('x' => 0.0, 'y' => 0.0, 'z' => 0.0, 'w' => 0.0));
+			$this->_x = $arr['dest']->getX() - $arr['orig']->getX();
+			$this->_y = $arr['dest']->getY() - $arr['orig']->getY();
+			$this->_z = $arr['dest']->getZ() - $arr['orig']->getZ();
+			if (($arr['dest']->getW()) !== NULL)
+				$this->_w = $arr['dest']->getW() - $arr['orig']->getW();
+			if (self::$verbose == true)
+			{
+				printf("Vector( x:%.2f, y:%.2f, z:%.2f, w:%.2f ) constructed" . PHP_EOL,
+					$this->_x, $this->_y, $this->_z, $this->_w);
+			}
+		}
+		function __toString():string{
+			$str = sprintf("Vector( x:%.2f, y:%.2f, z:%.2f, w:%.2f",
+					$this->_x, $this->_y, $this->_z, $this->_w);
+			if (self::$verbose === true && isset($this->col))
+				$str = $str.", ".$this->_col;
+			$str .= " )";
+			return $str;
+		}
+		function __destruct(){
+			if (self::$verbose == true)
+			{
+				printf("Vector( x:%.2f, y:%.2f, z:%.2f, w:%.2f",
+						$this->_x, $this->_y, $this->_z, $this->_w);
+				if (isset($this->_col))
+					print ", ".$this->_col;
+				print " ) destructed\n";
+			}
+		}
+		static function doc(){
+			if (file_exists("Vector.doc.txt"))
+				echo file_get_contents("Vector.doc.txt");
+		}
+		function magnitude():float{
+			return (sqrt($this->_x**2 + $this->_y**2 + $this->_z**2));
+		}
+
+		function normalize():Vector{
+			$mag = $this->magnitude();
+			$x = $this->_x / $mag;
+			$y = $this->_y / $mag;
+			$z = $this->_z / $mag;
+			$new = new Vertex(array('x' => $x, 'y' => $y, 'z' => $z));
+			$new = new Vector(array('dest' => $new));
+			return ($new);
+		}
+
+		function add(Vector $other):Vector{
+			$x = $this->_x + $other->getX();
+			$y = $this->_y + $other->getY();
+			$z = $this->_z + $other->getZ();
+			$new = new Vertex(array('x' => $x, 'y' => $y, 'z' => $z));
+			$new = new Vector(array('dest' => $new));
+			return $new;
+		}
+		function sub(Vector $other):Vector{
+			$x = $this->_x - $other->getX();
+			$y = $this->_y - $other->getY();
+			$z = $this->_z - $other->getZ();
+			$new = new Vertex(array('x' => $x, 'y' => $y, 'z' => $z));
+			$new = new Vector(array('dest' => $new));
+			return $new;
+		}
+		function opposite():Vector{
+			$x = -$this->getX();
+			$y = -$this->getY();
+			$z = -$this->getZ();
+			$new = new Vertex(array('x' => $x, 'y' => $y, 'z' => $z));
+			$new = new Vector(array('dest' => $new));
+			return $new;
+		}
+		function scalarProduct($k):Vector{
+			$x = $k * $this->getX();
+			$y = $k * $this->getY();
+			$z = $k * $this->getZ();
+			$new = new Vertex(array('x' => $x, 'y' => $y, 'z' => $z));
+			$new = new Vector(array('dest' => $new));
+			return $new;
+		}
+		function dotProduct(Vector $rhs):float{
+			$x = $rhs->getX() * $this->getX();
+			$y = $rhs->getY() * $this->getY();
+			$z = $rhs->getZ() * $this->getZ();
+			return ($x + $y + $z);
+		}
+		function crossProduct(Vector $rhs):Vector{
+			$x = $this->getY() * $rhs->getZ() - $rhs->getY() * $this->getZ();
+			$y = $this->getZ() * $rhs->getX() - $rhs->getZ() * $this->getX();
+			$z = $this->getX() * $rhs->getY() - $rhs->getX() * $this->getY();
+			$new = new Vertex(array('x' => $x, 'y' => $y, 'z' => $z));
+			$new = new Vector(array('dest' => $new));
+			return ($new);
+		}
+		function cos(Vector $rhs):float{
+			return $this->dotProduct($rhs) / ($this->magnitude() * $rhs->magnitude()); 
+		}
+
+	}
+?>
