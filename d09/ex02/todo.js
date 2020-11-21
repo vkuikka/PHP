bacac85c4e4c4827554f1c0ef62ebd32c14fa051
vkuikka <villekuikka1@gmail.com>
2020-11-21 14:45:17 +0200

    got todo list working

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -2,16 +2,14 @@ var		ft_list;
 var		cookie = [];
 
 window.onload = function () {
-	deleteAllCookies();
 	document.querySelector("#newTodo").addEventListener("click", newTodo);
 	ft_list = document.querySelector("#ft_list");
 	var tmp = document.cookie;
-	console.log(tmp);
+	tmp = tmp.substring(0, tmp.indexOf(";"));
 	if (tmp)
 	{
 		cookie = JSON.parse(tmp);
-		cookie.forEach(function(task){addTodo(task);});
-		// cookie.forEach(task => console.log(task));
+		cookie.forEach(task => newTodo(task));
 	}
 }
 
@@ -21,11 +19,13 @@ window.onunload = function () {
     for (var i = 0; i < todo.length; i++)
         newCookie.unshift(todo[i].innerHTML);
     document.cookie = JSON.stringify(newCookie);
-};
+}
 
-function newTodo() {
-	console.log("newtodo");
-	var todo = prompt("give todo");
+function newTodo(arg) {
+	if (arg && typeof(arg) != "object")
+		todo = arg;
+	else
+		var todo = prompt("give task");
 	if (todo == '')
 		return;
 	var newdiv = document.createElement("div");
@@ -33,19 +33,9 @@ function newTodo() {
 	newdiv.addEventListener("click", deleteTodo);
 	ft_list.insertBefore(newdiv, ft_list.firstChild);
 }
+
 function deleteTodo() {
 	if (confirm("confirm")){
 		this.parentElement.removeChild(this);
 	}
-}
-
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
 }
